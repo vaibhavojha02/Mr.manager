cfb69ef11e1304831a9f6b6085eb96830386ab41
vaibhav ojha <vaibhavojha34@gmail.com>
2024-03-22 10:14:58 +0530

    svg icon added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,9 +8,7 @@ import Footer from "./components/Footer";
 function App() {
   return (
     <>
-      <div className=" fixed inset-0 -z-10 h-screen  w-screen items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
-
-      <Navbar />
+       <div className=" fixed inset-0 -z-10 h-screen  w-screen items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
       <div className="min-h-[90vh]">
         <Manager />
       </div>
